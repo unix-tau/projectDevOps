bc17a3ab1bcd33db6b5c357cc5a2d8659b0feca8
tau <murindataurai@gmail.com>
2024-03-01 13:19:24 +0200

    update

--- springProjects/restful-web-service/src/main/java/com/reify/restfulwebservice/designpatterns/behavioural/Command/Test.java
+++ springProjects/restful-web-service/src/main/java/com/reify/restfulwebservice/designpatterns/behavioural/Command/Test.java
@@ -1,11 +1,7 @@
 package com.reify.restfulwebservice.designpatterns.behavioural.Command;
 
 
-import com.reify.restfulwebservice.designpatterns.behavioural.Command.Light;
-import com.reify.restfulwebservice.designpatterns.behavioural.Command.LightOnCommand;
-import com.reify.restfulwebservice.designpatterns.behavioural.Command.RemoteControl;
-
-public class RestfulWebServiceApplication {
+public class Test {
 
 	public static void main(String[] args) {
 		RemoteControl remoteControl = new RemoteControl();
